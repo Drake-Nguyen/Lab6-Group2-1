b5181dac4a00a9b471191fa42bb944402ca69154
dangkyle <dangkyle@csu.fullerton.edu>
2023-03-04 23:01:39 -0800

    edited out repeating test

--- tests/test_id.hpp
+++ tests/test_id.hpp
@@ -12,11 +12,10 @@ static void test_id (void) {
    // test (procedure_IDs, "",{}, CURRENT_LOCATION);
    test (procedure_IDs, "a", {{"Identifier", "a"}}, CURRENT_LOCATION);
    test (procedure_IDs, "a,b", {{"Identifier", "a"}, {"Separator", ","}, {"Identifier", "b"}}, CURRENT_LOCATION);
-   test (procedure_IDs, "a+b", {{"Identifier", "a"}, {"Operator", "+"}, {"Identifier", "b"}}, CURRENT_LOCATION);
+   test (procedure_IDs, "a+B", {{"Identifier", "a"}, {"Operator", "+"}, {"Identifier", "B"}}, CURRENT_LOCATION);
    test (procedure_IDs, "class", {{"Keyword", "class"}}, CURRENT_LOCATION);
    test (procedure_IDs, "", {}, CURRENT_LOCATION);
    test (procedure_IDs, "a b", {{"Identifier", "a"}, {"Other Separators", " "}, {"Identifier", "b"}}, CURRENT_LOCATION);
-   test (procedure_IDs, "a a", {{"Identifier", "a"}, {"Other Separators", " "}, {"Identifier", "a"}}, CURRENT_LOCATION);
    test (procedure_IDs, "    ", {}, CURRENT_LOCATION);
    test (procedure_IDs, "$tring", {{"Identifier", "$tring"}}, CURRENT_LOCATION);
    
