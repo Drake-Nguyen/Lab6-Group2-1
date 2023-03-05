adb979d450b04ce25c919e0ecff400ca0f3cd96e
Diamond Dinh <diamondburned@csu.fullerton.edu>
2023-03-04 20:46:06 -0800

    Add tests for Condition

---
+++ tests/test_condition.hpp
@@ -0,0 +1,35 @@
+#include "../lexer.hpp"
+#include "../procedure_functions.h"
+
+#include "./utils.hpp"
+#define macro TEST_NO_MAIN
+
+#include <map>
+#include <sstream>
+
+static void test_condition(void) {
+  const auto t = [](std::string lhs, std::string op, std::string rhs,
+                    const char *file, int line) {
+    test(procedure_Condition, lhs + op + rhs,
+         {
+             {"Keyword", lhs},
+             {"Operator", op},
+             {"Keyword", rhs},
+         },
+         file, line);
+    test(procedure_Condition, lhs + " " + op + " " + rhs,
+         {
+             {"Keyword", lhs},
+             {"Other Separators", " "},
+             {"Operator", op},
+             {"Other Separators", " "},
+             {"Keyword", rhs},
+         },
+         file, line);
+  };
+
+  t("true", "==", "true", CURRENT_LOCATION);
+  t("true", "!=", "false", CURRENT_LOCATION);
+  t("123", "<", "345", CURRENT_LOCATION);
+  t("456", ">", "123", CURRENT_LOCATION);
+}
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,6 @@
 #include "../include/acutest.h"
 
+#include "./test_condition.hpp"
 #include "./test_qualifier.hpp"
 #include "./test_relop.hpp"
 
@@ -7,5 +8,6 @@ TEST_LIST = {
     {"qualifier_boolean", test_qualifier_boolean},
     {"qualifier_int", test_qualifier_int},
     {"relop", test_relop},
+    {"condition", test_condition},
     {},
 };
