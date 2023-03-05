dee457ee91b3fb3945047dbaf07d7f69049c432a
Diamond Dinh <diamondburned@csu.fullerton.edu>
2023-03-04 20:24:37 -0800

    Add tests for Relop

---
+++ tests/test_relop.hpp
@@ -0,0 +1,18 @@
+#include "../lexer.hpp"
+#include "../procedure_functions.h"
+
+#include "./utils.hpp"
+#define macro TEST_NO_MAIN
+
+#include <map>
+#include <sstream>
+
+static void test_relop(void) {
+  test(procedure_Relop, "==", {{"Operator", "=="}}, CURRENT_LOCATION);
+  test(procedure_Relop, "!=", {{"Operator", "!="}}, CURRENT_LOCATION);
+  test(procedure_Relop, "<", {{"Operator", "<"}}, CURRENT_LOCATION);
+  test(procedure_Relop, "<=", {{"Operator", "<="}}, CURRENT_LOCATION);
+  test(procedure_Relop, ">", {{"Operator", ">"}}, CURRENT_LOCATION);
+  test(procedure_Relop, ">=", {{"Operator", ">"}, {"Operator", "="}},
+       CURRENT_LOCATION);
+}
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,9 +1,11 @@
 #include "../include/acutest.h"
 
 #include "./test_qualifier.hpp"
+#include "./test_relop.hpp"
 
 TEST_LIST = {
     {"qualifier_boolean", test_qualifier_boolean},
     {"qualifier_int", test_qualifier_int},
+    {"relop", test_relop},
     {},
 };
