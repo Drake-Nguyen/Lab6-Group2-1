bac8e9e025d50fe61da43d3f92b68e468fc824a2
dangkyle <dangkyle@csu.fullerton.edu>
2023-03-04 23:16:50 -0800

    qualifier test and result changes

--- tests/test_qualifier.hpp
+++ tests/test_qualifier.hpp
@@ -11,10 +11,10 @@ static void test_qualifier (void) {
 
   test (procedure_Qualifier, "static", {{"Keyword", "static"}}, CURRENT_LOCATION);
   test (procedure_Qualifier, "*", {{"Operator", "*"}}, CURRENT_LOCATION);
-  test (procedure_Qualifier, "&", {{"Operator", "&"}}, CURRENT_LOCATION);
+  test (procedure_Qualifier, "&", {{"Other Separators", "&"}}, CURRENT_LOCATION);
   test (procedure_Qualifier, "unsigned", {{"Keyword", "unsigned"}}, CURRENT_LOCATION);
   test (procedure_Qualifier, "int", {{"Keyword", "int"}}, CURRENT_LOCATION);
-  test (procedure_Qualifier, "string", {{"Keyword", "string"}}, CURRENT_LOCATION);
+  test (procedure_Qualifier, "char", {{"Keyword", "char"}}, CURRENT_LOCATION);
 
   // Thanks to Diamond Dinh for these test cases
   test (procedure_Qualifier, "5", {{"Integer", "5"}}, CURRENT_LOCATION);
