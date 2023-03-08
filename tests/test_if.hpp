d8ede16fa143673d42688643fdcf690a86499c73
Diamond Dinh <diamondburned@csu.fullerton.edu>
2023-03-07 23:41:10 -0800

    Add tests for If

--- tests/test_if.hpp
+++ tests/test_if.hpp
@@ -11,57 +11,63 @@ static void test_if(void) {
   // no else
 
   // statement for compound
-  test(procedure_If, "if(1==1){ return; } fi", {{}}, CURRENT_LOCATION);
-  test(procedure_If, "if (1==1) { return; a = b; }", {}, CURRENT_LOCATION);
-  test(procedure_If, "if (1==1) { a = b; }", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) { return; } fi", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) { return; a = b; } fi", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) { a = b; } fi", {}, CURRENT_LOCATION);
 
   // statement for assign
-  test(procedure_If, "if (1==1) a = b; fi", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) a = b; fi", {}, CURRENT_LOCATION);
 
   // statement for (nested) if
-  test(procedure_If, "if (1==1) if (false) a = b;", {}, CURRENT_LOCATION);
-  test(procedure_If, "if (1==1) { if (false) return; }", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) if (0 == 1) a = b; fi fi", {},
+       CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) { if (0 == 1) return; fi } fi", {},
+       CURRENT_LOCATION);
 
   // statement for return
-  test(procedure_If, "if (1==1) return;", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) return; fi", {}, CURRENT_LOCATION);
 
   // statement for print
-  test(procedure_If, "if (1==1) put('h');", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) put(h); fi", {}, CURRENT_LOCATION);
 
   // statement for scan
-  test(procedure_If, "if (1==1) get(a);", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) get(a); fi", {}, CURRENT_LOCATION);
 
   // statement for while
-  test(procedure_If, "if (1==1) while (false) a = b;", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) while (0 == 1) a = b; fi", {},
+       CURRENT_LOCATION);
 
   // with else
 
   // statement for compound
-  test(procedure_If, "if (1==1) { return; } else { return false; }", {},
+  test(procedure_If, "if (1 == 1) { return; } else { return false; } fi", {},
        CURRENT_LOCATION);
-  test(procedure_If, "if (1==1) { return; a = b; } else { return false; }", {},
-       CURRENT_LOCATION);
-  test(procedure_If, "if (1==1) { a = b; } else { return false; }", {},
+  test(procedure_If, "if (1 == 1) { return; a = b; } else { return false; } fi",
+       {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) { a = b; } else { return false; } fi", {},
        CURRENT_LOCATION);
 
   // statement for assign
-  test(procedure_If, "if (1==1) a = b; else a = c;", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) a = b; else a = c; fi", {}, CURRENT_LOCATION);
 
   // statement for (nested) if
-  test(procedure_If, "if (1==1) if (false) a = b; else a = c;", {},
+  test(procedure_If, "if (1 == 1) if (0 == 1) a = b; else a = c; fi fi", {},
        CURRENT_LOCATION);
 
   // statement for return
-  test(procedure_If, "if (1==1) return; else return;", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) return; else return; fi", {},
+       CURRENT_LOCATION);
 
   // statement for print
-  test(procedure_If, "if (1==1) put('h'); else put('e');", {},
+  test(procedure_If, "if (1 == 1) put(h); else put(e); fi", {},
        CURRENT_LOCATION);
 
   // statement for scan
-  test(procedure_If, "if (1==1) get(a); else get(b);", {}, CURRENT_LOCATION);
+  test(procedure_If, "if (1 == 1) get(a); else get(b); fi", {},
+       CURRENT_LOCATION);
 
   // statement for while
-  test(procedure_If, "if (1==1) while (false) a = b; else while (false) a = c;",
-       {}, CURRENT_LOCATION);
+  test(procedure_If,
+       "if (1 == 1) while (0 == 1) a = b; else while (1 == 1) a = c; fi", {},
+       CURRENT_LOCATION);
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,21 +1,23 @@
 #include "../include/acutest.h"
 
+#include "./test_assign.hpp"
 #include "./test_condition.hpp"
+#include "./test_declaration.hpp"
+#include "./test_expression.hpp"
 #include "./test_id.hpp"
+#include "./test_if.hpp"
+#include "./test_parameter.hpp"
+#include "./test_parameter_list.hpp"
+#include "./test_primary.hpp"
 #include "./test_qualifier.hpp"
 #include "./test_relop.hpp"
-#include "./test_assign.hpp"
-#include "./test_primary.hpp"
-#include "./test_expression.hpp"
-#include "./test_parameter.hpp"
-#include "./test_term.hpp"
 #include "./test_return.hpp"
-#include "./test_declaration.hpp"
-#include "./test_parameter_list.hpp"
+#include "./test_term.hpp"
 
 TEST_LIST = {
     {"relop", test_relop},
     {"condition", test_condition},
+    {"if", test_if},
     {"id", test_id},
     {"qualifier", test_qualifier},
     {"assign", test_assign},
