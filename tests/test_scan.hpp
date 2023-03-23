2aec2988d9e1a3cae3077ed0a0c70a9c8af41ea5
jonathanlam10 <jonathanlam10@csu.fullerton.edu>
2023-03-22 19:23:12 -0700

    update file to camel case format

--- tests/test_scan.hpp
+++ tests/test_scan.hpp
@@ -8,13 +8,16 @@
 #include <sstream>
 
 static void test_scan(void) {
-  test(procedureScan, "get(a);", {{"Function", "get"}, {"Seperator", "("}, {"Identifier", "a"}, {"Seperator", ")"}, {"Seperator", ";"}}, CURRENT_LOCATION);
-  test(procedureScan, "get(a,b);", {{"Function", "get"}, {"Seperator", "("}, {"Identifier", "a"}, 
-                                    {"Seperator", ","}, {"Identifier", "b"}, {"Seperator", ")"}, {"Seperator", ";"}}, CURRENT_LOCATION);
-  test(procedureScan, "get(a+b);", {{"Function", "get"}, {"Seperator", "("}, {"Identifier", "a"}, 
-                                    {"Operator", "+"}, {"Identifier", "b"}, {"Seperator", ")"}, {"Seperator", ";"}}, CURRENT_LOCATION);
+test(procedureScan, "get(a);", {{"Function", "get"}, {"Seperator", "("}, 
+{"Identifier", "a"}, {"Seperator", ")"}, {"Seperator", ";"}}, CURRENT_LOCATION);
+test(procedureScan, "get(a,b);",{{"Function", "get"}, {"Seperator", "("}, 
+{"Identifier", "a"}, {"Seperator", ","}, {"Identifier", "b"}, {"Seperator", ")"}, 
+{"Seperator", ";"}}, CURRENT_LOCATION);
+test(procedureScan, "get(a+b);", {{"Function", "get"}, {"Seperator", "("}, 
+{"Identifier", "a"}, {"Operator", "+"}, {"Identifier", "b"}, {"Seperator", ")"}, 
+{"Seperator", ";"}}, CURRENT_LOCATION);
   
-  test_fail(procedureScan, "get a;", {}, CURRENT_LOCATION);
-  test_fail(procedureScan, "get a+b;", {}, CURRENT_LOCATION);
-  test_fail(procedureScan, "get a,b;", {}, CURRENT_LOCATION);
+test_fail(procedureScan, "get a;", {}, CURRENT_LOCATION);
+test_fail(procedureScan, "get a+b;", {}, CURRENT_LOCATION);
+test_fail(procedureScan, "get a,b;", {}, CURRENT_LOCATION);
 }
