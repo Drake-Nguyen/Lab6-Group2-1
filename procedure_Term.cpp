c21ca4cfff6bcc3c46abc8392ad08605341220e5
Long Calamia <long93933@gmail.com>
2023-03-22 20:20:21 -0700

    removed return holder for procedure return, term, and primary

--- procedure_Term.cpp
+++ procedure_Term.cpp
@@ -11,7 +11,7 @@ bool procedureTerm(vector<token_323> &all_tokens, int &loc) {
       all_tokens[loc_helper].rule_holder.begin();
 
   // get token and test rule
-  bool return_holder = false;
+  
   token_323 token_holder;
   token_holder = all_tokens[loc];
   if (procedureFactor(all_tokens, loc)) {
@@ -21,12 +21,10 @@ bool procedureTerm(vector<token_323> &all_tokens, int &loc) {
       all_tokens[loc_helper].rule_holder.push_back(output_str);
       // std::cerr << output_str;
 
-      return_holder = true;
+      
     } else {
       throw std::logic_error("error, expecting <Term>'");
     }
-  } else {
-    return_holder = false;
-  }
-  return return_holder;
+  } 
+  return false;
 }
